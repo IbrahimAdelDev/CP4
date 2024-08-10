9e74834b90a4d89e4153a5960ba4ec78d4242c8f
ibrahimadel <125532490+IbrahimAdel9927@users.noreply.github.com>
2024-08-10 09:45:39 +0300

    Create Kattis_thelastproblem.cpp

---
+++ Kattis_thelastproblem.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+// #include <string>
+
+// using namespace std;
+
+void solve(){
+    char s[6000];
+    scanf("%[a-zA-Z ]", &s);
+    printf("Thank you, %s, and farewell!", s);
+}
+
+int main(){
+    solve();
+    return 0;
+}
