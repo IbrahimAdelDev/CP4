fb69ba9b6f53d7c87959a3a009e061ccdf62305c
ibrahimadel <125532490+IbrahimAdel9927@users.noreply.github.com>
2024-08-10 14:15:15 +0300

    Create Kattis_faktor.cpp

---
+++ Kattis_faktor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// #include <math.h>
+#include <string>
+#include <vector>
+
+// using namespace std;
+
+void solve(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    b--;
+    printf("%d", a*b+1);
+}
+
+int main(){
+    solve();
+    return 0;
+}
