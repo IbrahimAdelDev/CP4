3b2dbe7209174de7efe7a5c4ba172630399c5683
ibrahimadel <125532490+IbrahimAdel9927@users.noreply.github.com>
2024-08-10 09:26:47 +0300

    Create Kattis_r2.cpp

---
+++ Kattis_r2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+// #include <string>
+
+// using namespace std;
+
+void solve(){
+    int r, s;
+    scanf("%d %d", &r, &s);
+    printf("%d", 2*s-r);
+}
+
+int main(){
+    solve();
+    return 0;
+}
