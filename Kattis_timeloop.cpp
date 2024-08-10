acd9e64d129c697fa918b8d0c81e8502e04e7bcc
ibrahimadel <125532490+IbrahimAdel9927@users.noreply.github.com>
2024-08-10 19:05:55 +0300

    Create Kattis_timeloop.cpp

---
+++ Kattis_timeloop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+// #include <string>
+// #include <vector>
+
+// using namespace std;
+
+void solve(){
+    int a;
+    scanf("%d", &a);
+    for(int i=1 ; i<=a ; i++){
+        printf("%d Abracadabra\n", i);
+    }
+}
+
+int main(){
+    solve();
+    return 0;
+}
