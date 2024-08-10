69927bc98abb5b36cfcc37783e785ef6c36cfed4
ibrahimadel <125532490+IbrahimAdel9927@users.noreply.github.com>
2024-08-10 06:06:05 +0300

    Create UVA_13025.cpp

---
+++ UVA_13025.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+// #include <string>
+
+// using namespace std;
+
+void solve(){
+    printf("May 29, 2013 Wednesday\n");
+}
+
+int main(){
+    solve();
+    return 0;
+}
