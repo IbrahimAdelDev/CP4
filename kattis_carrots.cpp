d5f80543b5985c32d16dca17f5f93f581144773f
ibrahimadel <125532490+IbrahimAdel9927@users.noreply.github.com>
2024-08-10 06:04:54 +0300

    Create kattis_carrots.cpp

---
+++ kattis_carrots.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+// #include <string>
+
+// using namespace std;
+
+void solve(){
+    int n, p;
+    scanf("%d %d", &n, &p);
+    while(n--){
+        char s[1000];
+        scanf("%s", s);
+    }
+    printf("%d", p);
+}
+
+int main(){
+    solve();
+    return 0;
+}
