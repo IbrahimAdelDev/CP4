fc5ca32c4a97bacaaa9ae4501b354b37dd9df213
ibrahimadel <125532490+IbrahimAdel9927@users.noreply.github.com>
2024-08-11 19:43:04 +0300

    Create UVA_01124.cpp

---
+++ UVA_01124.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+// #include <string>
+// #include <vector>
+
+// using namespace std;
+
+void solve(){
+    char a[1000];
+    while(scanf("%[A-Z0-9 =]", a)!=EOF){
+        printf("%s\n", a);
+    }
+}
+
+int main(){
+    solve();
+    return 0;
+}
